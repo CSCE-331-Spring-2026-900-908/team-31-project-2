b3d90a8c91f7b493bb264fa9544af291bcc2db41
Long Vo <longvo.work84@gmail.com>
2026-02-17 09:58:57 -0600

    finished Special Query #3

--- queries.sql
+++ queries.sql
@@ -3,6 +3,13 @@
 -- Special Query #2: "Peak Sales Day"
 
 -- Special Query #3: "Realistic Sales History"
+SELECT
+    TO_CHAR(created_at, 'HH12AM') AS hour_label,
+    COUNT(*) AS num_orders,
+    TRUNC(SUM(total_final)::NUMERIC, 2) AS total_sales
+FROM "order"
+GROUP BY hour_label
+ORDER BY hour_label;
 
 -- Special Query #4: Menu Item Inventory
 SELECT p.name AS menu_item, COUNT(pi.item_id) AS inventory_item_count
