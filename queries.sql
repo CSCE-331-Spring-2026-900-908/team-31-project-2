3b575e2b221a3103a9cbe1ef6a9491fc773c3064
Long Vo <longvo.work84@gmail.com>
2026-02-17 10:44:55 -0600

    finished Peak Sales Day

--- queries.sql
+++ queries.sql
@@ -1,15 +1,32 @@
 -- Special Query #1: "Weekly Sales History"
 
 -- Special Query #2: "Peak Sales Day"
+WITH top_orders AS (
+    SELECT
+        DATE(created_at) as order_day,
+        total_final,
+        ROW_NUMBER() OVER (
+            PARTITION BY DATE(created_at)
+            ORDER BY total_final DESC
+        ) as rn
+    FROM "order"
+)
+SELECT 
+    order_day,
+    TRUNC(SUM(total_final)::NUMERIC, 2) as top10_total
+FROM top_orders
+WHERE rn <= 10
+GROUP BY order_day
+ORDER BY order_day ASC;
 
 -- Special Query #3: "Realistic Sales History"
 SELECT
-    TO_CHAR(created_at, 'HH12AM') AS hour_label,
+    TO_CHAR(created_at, 'FMHH12AM') AS hour_label,
     COUNT(*) AS num_orders,
     TRUNC(SUM(total_final)::NUMERIC, 2) AS total_sales
 FROM "order"
-GROUP BY hour_label
-ORDER BY hour_label;
+GROUP BY hour_label, EXTRACT(HOUR FROM created_at)
+ORDER BY EXTRACT(HOUR FROM created_at) ASC;
 
 -- Special Query #4: Menu Item Inventory
 SELECT p.name AS menu_item, COUNT(pi.item_id) AS inventory_item_count
