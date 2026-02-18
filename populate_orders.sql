49d797b231383513851d969bef4ee8d9feaa9b0b
NoahHig <noahhigpen@gmail.com>
2026-02-18 11:26:23 -0600

    Added files to populate orders

---
+++ populate_orders.sql
@@ -0,0 +1,3 @@
+\copy "order" from 'C:/Users/noahh/Desktop/CSCE331/team-31-project-2/orders.csv' CSV HEADER
+\copy orderdetail from 'C:/Users/noahh/Desktop/CSCE331/team-31-project-2/order_details.csv' CSV HEADER
+\copy ordermodifier from 'C:/Users/noahh/Desktop/CSCE331/team-31-project-2/order_modifiers.csv' CSV HEADER
