45ac4e7bd31fea507a8ee74d29a95f1cde1bb428
“abelasteway” <astewayabel@gmail.com>
2026-02-17 21:11:55 -0600

    changed the names of my queries

--- AbelsQueries/normQueries3.sql
+++
@@ -1,2 +0,0 @@
-SELECT 'Which modifier appears the least in July'
-SELECT snapshot_name, COUNT(*) AS times_sold FROM "order" JOIN ordermodifier ON "order".id = ordermodifier.order_detail_id WHERE "order".created_at >= DATE '2026-07-01' AND "order".created_at < DATE '2026-08-01'  GROUP BY snapshot_name ORDER BY times_sold ASC LIMIT 1; 
--- AbelsQueries/normQueries4.sql
+++
@@ -1,4 +0,0 @@
-SELECT 'A list of the most expensive purchase in Novemeber';
-SELECT total_final FROM "order"  
-WHERE "order".created_at >= DATE '2026-11-01' AND "order".created_at < DATE '2026-12-01' 
-ORDER BY total_final  DESC LIMIT (10);
