e0d98ee177450c3e35d09e6f60f916ea7c2a07a4
Nitheesh K <nitishkoda@outlook.com>
2026-02-20 09:29:07 -0600

    Move SQL Files

--- special_queries/menu_item_inventory.sql
+++
@@ -1,6 +0,0 @@
--- Special Query #4: "Menu Item Inventory"
-SELECT p.name AS menu_item, COUNT(pi.item_id) AS inventory_item_count
-FROM product p
-JOIN productingredient pi ON p.product_id = pi.product_id
-GROUP BY p.product_id, p.name
-ORDER BY p.name;
