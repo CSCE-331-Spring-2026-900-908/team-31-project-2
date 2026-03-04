3b130c6c294ebf8a97f21a327a0da22b0cf88aa3
“abelasteway” <astewayabel@gmail.com>
2026-03-04 09:55:32 -0600

    Added commets for employees

--- src/main/java/com/example/team31project2/Employee.java
+++ src/main/java/com/example/team31project2/Employee.java
@@ -5,7 +5,13 @@ public class Employee {
     private String name;
     private String role;
     private String pinHash;
-
+       /*  
+        @param id: gets the id of the employee  
+        @param name: get name of employee 
+        @param role: get the role
+        @param pinHash: get the pin 
+        @returns nothing
+    */  
     public Employee(int id, String name, String role, String pinHash) {
         this.id = id;
         this.name = name;
@@ -13,22 +19,37 @@ public class Employee {
         this.pinHash = pinHash;
     }
 
+    /*  
+        @returns id
+    */  
     public int getId() {
         return id;
     }
 
+    /*  
+        @returns name
+    */  
     public String getName() {
         return name;
     }
 
+    /*  
+        @returns role
+    */  
     public String getRole() {
         return role;
     }
 
+    /*  
+        @returns pin
+    */  
     public String getPinHash() {
         return pinHash;
     }
 
+    /*  
+        @returns a boolean if it is a manager 
+    */  
     public boolean isManager() {
         return "Manager".equalsIgnoreCase(role) || "manager".equals(role);
     }
